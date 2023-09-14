cbb3bb342cb924d46e6a92b8171096a0bd49acbc
Rodlikespants <rodwittlai@gmail.com>
2023-09-14 17:59:46 -0400

    added useRef for accessibility and focusing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+
+
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    }, []);
+    return ref.current;
+}
 
 function Todo(props) {
     const [isEditing, setEditing] = useState(false);
     const [newName, setNewName] = useState("");
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
+    const wasEditing = usePrevious(isEditing);
+
+    useEffect(() => {
+        if (!wasEditing && isEditing) {
+            editFieldRef.current.focus();
+        }
+        if (wasEditing && !isEditing) {
+            editButtonRef.current.focus();
+        }
+    }, [isEditing, wasEditing]);
 
     function handleChange(e) {
         setNewName(e.target.value);
@@ -27,6 +48,7 @@ function Todo(props) {
                     type="text"
                     value={newName}
                     onChange={handleChange}
+                    ref={editFieldRef}
                 />
             </div>
             <div className="btn-group">
@@ -66,6 +88,7 @@ function Todo(props) {
                 type="button"
                 className="btn"
                 onClick={() => setEditing(true)}
+                ref={editButtonRef}
             >
                 Edit <span className="visually-hidden">{props.name}</span>
             </button>
